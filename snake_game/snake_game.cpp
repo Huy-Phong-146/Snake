6c1c7aa605eaa5ba84a32a230c98526f3ab6f5ad
24521907-minhtu <24521907@gm.uit.edu.vn>
2025-11-13 18:16:21 +0700

    Tính năng ăn táo

--- snake_game/snake_game.cpp
+++ snake_game/snake_game.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <time.h>
 
 using namespace std;
 
@@ -13,7 +14,12 @@ struct Point {
     int y;
 
     Point() {}
+
     Point(const int& x, const int& y) : x(x), y(y) {}
+
+    bool operator==(const Point& other) {
+        return x == other.x && y == other.y;
+    }
 };
 
 class Snake {
@@ -33,7 +39,9 @@ public:
         }
     }
 
-    void move(const char& direction) {
+    bool move(const char& direction, const Point& apple) {
+        Point tail = body.back();
+
         for (int i = length - 1; i > 0; --i)
             body[i] = body[i - 1];
 
@@ -43,15 +51,16 @@ public:
         case 'a': body[0].x--; break;
         case 's': body[0].y++; break;
         }
+
+        if (body[0] == apple) {
+            body.emplace_back(tail);
+            length++;
+            return true;
+        }
+
+        return false;
     }
 };
-
-void gotoxy(int column, int line) {
-    COORD coord;
-    coord.X = column;
-    coord.Y = line;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
 
 class GamePlay {
 private:
@@ -61,32 +70,43 @@ private:
     int MAX_Y = 20;
 
     Snake arbok;
-
-public:
-    GamePlay() {}
+    Point apple;
 
     void draw_barrier() {
-        for (int i = MIN_X; i <= MAX_X; i++) {
-            for (int j = MIN_Y; j <= MAX_Y; j++) {
+        for (int i = MIN_X; i <= MAX_X; i++)
+            for (int j = MIN_X; j <= MAX_Y; j++)
                 if ((i == MIN_X) || (i == MAX_X) || (j == MIN_Y) || (j == MAX_Y)) {
                     gotoxy(i, j);
                     printf("*");
                 }
-            }
-        }
     }
+
+    void generate_apple() {
+        apple.x = rand() % (MAX_X - MIN_X) + MIN_X;
+        apple.y = rand() % (MAX_Y - MIN_Y) + MIN_Y;
+    }
+
+    void display() {
+        system("cls");
+        draw_barrier();
+        arbok.display();
+        gotoxy(apple.x, apple.y); cout << "O";
+    }
+
+public:
+    GamePlay() {}
 
     void run() {
         char direction = 'd';
+        srand(time(0));
+        generate_apple();
 
         while (true) {
             if (kbhit())
                 direction = getch();
 
-            system("cls");
-            draw_barrier();
-            arbok.display();
-            arbok.move(direction);
+            display();
+            if (arbok.move(direction, apple)) generate_apple();
             Sleep(150);
         }
     }
@@ -97,4 +117,11 @@ int main() {
     snake_game.run();
 
     return 0;
+}
+
+void gotoxy(int column, int line) {
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
